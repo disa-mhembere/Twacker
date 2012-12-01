7bd8a50ae5fdf2d9ddebe344683b60856386cd40
Alex Long <alexander.long91@gmail.com>
2012-11-30 22:53:59 -0500

    Fixed Histogram

--- src/edu/jhu/twacker/client/view/HomeView.java
+++ src/edu/jhu/twacker/client/view/HomeView.java
@@ -7,13 +7,11 @@ package edu.jhu.twacker.client.view;
 import java.util.LinkedList;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.core.client.JsArray;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
@@ -26,8 +24,6 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.visualization.client.AbstractDataTable;
 import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
-import com.google.gwt.visualization.client.Selection;
-import com.google.gwt.visualization.client.events.SelectHandler;
 import com.google.gwt.visualization.client.visualizations.corechart.AxisOptions;
 import com.google.gwt.visualization.client.visualizations.corechart.LineChart;
 import com.google.gwt.visualization.client.visualizations.corechart.Options;
@@ -63,10 +59,9 @@ public class HomeView extends View {
 	private Button searchButton = new Button("Search");
 	
 	private Label infoLabel = new Label();
-	private Label errLabel = new Label();
 	
-	private LineChart line;
-	private PieChart pie;
+	private static DataTable table;
+	private static int count = 0;
 
 	//	private List<Search> searchTerms; // TODO : DM
 
@@ -101,7 +96,6 @@ public class HomeView extends View {
 		
 		mainPanel.add(searchPanel);
 		mainPanel.add(infoLabel);
-		mainPanel.add(errLabel);
 		mainPanel.add(saveStatusLabel);
 		
 		resultsTab.add(histogramPanel, "Histogram");
@@ -169,42 +163,32 @@ public class HomeView extends View {
 			 * Send the name from the nameField to
 			 *  the server and wait for a response.
 			 */
-			private void sendQueryToServer(LinkedList<String> terms) {
-				infoLabel.setText("Gathering Data!");
-				errLabel.setText("");
-				histogramPanel.clear();
-				sentimentPanel.clear();
-				for (String s : terms) {
+			private void sendQueryToServer(final LinkedList<String> searchTerms) {
+				infoLabel.setText("Gathering Data!");				
+				
+				table = DataTable.create();
+				table.addColumn(ColumnType.NUMBER, "Day");
+				table.addRows(10);
+				count = 0;
+				
+				for (String s : searchTerms) {
 					queryService.queryServer(s, new AsyncCallback<String>() {
 						public void onFailure(Throwable caught) {
-							errLabel.setText(caught.toString());
 						}
 
 						public void onSuccess(String result) {
-							if (pie == null && line == null) {
-								line = new LineChart(createLineGraphs(result), createOptions(result));
-								line.addSelectHandler(createSelectHandler(line));
-
-								pie = new PieChart(createPieChart(result), createOptions(result));
-								pie.addSelectHandler(createSelectHandler(pie));
-
-								histogramPanel.add(line);
-								sentimentPanel.add(pie);
-								resultsTab.setVisible(true);
-								resultsTab.selectTab(0);							
+							infoLabel.setText("");
+							if (count == 0 ) {
+								sentimentPanel.clear();
+								histogramPanel.clear();
 							}
-							else {
-//								line.draw(createLineGraphs(result), createOptions());
-//								pie.draw(createPieChart(result), createOptions());
-								line = new LineChart(createLineGraphs(result), createOptions(result));
-								line.addSelectHandler(createSelectHandler(line));
-
-								pie = new PieChart(createPieChart(result), createOptions(result));
-								pie.addSelectHandler(createSelectHandler(pie));
-
-								histogramPanel.add(line);
-								sentimentPanel.add(pie);
-								resultsTab.selectTab(0);
+							
+							updateTable(result);
+							sentimentPanel.add(new PieChart(createPieChart(result), createOptions(result)));
+							if (count == searchTerms.size()) { 
+								histogramPanel.add(new LineChart(table, createOptions(result)));
+								resultsTab.setVisible(true);
+								resultsTab.selectTab(0);								
 							}
 						}
 					});
@@ -219,7 +203,25 @@ public class HomeView extends View {
 		searchBox2.addKeyUpHandler(handler);
 		searchBox3.addKeyUpHandler(handler);
 	}
+	
+	public static void updateTable(String s) {
+		String histogram = s.substring(s.indexOf("histogram"), s.indexOf("sentiment"));
+		String data = histogram.substring(histogram.indexOf("data"), histogram.indexOf("]"));
+		String[] days = data.split(",");
+		int[] counts = new int[days.length];
+		for (int i = 0; i < days.length; i++) {
+			counts[i] = Integer.parseInt(days[i].replaceAll("\\D", ""));
+		}
+		table.addColumn(ColumnType.NUMBER, s.substring(s.indexOf(": \""), s.indexOf("\",")));
 
+		for (int i = 0; i < counts.length; i++) {
+			if (count == 0)
+				table.setValue(i, 0, i);
+			table.setValue(i, count+1, counts[i]);
+		}
+		count++;
+	}
+	
 	/**
 	 * Save search Terms(s) provided to users profile for {@link PersonalHistoryView}
 	 * @param searchTerm
@@ -248,7 +250,7 @@ public class HomeView extends View {
 	private Options createOptions(String result) {
 		Options options = Options.create();
 		options.setWidth(400);
-		options.setHeight(240);
+		options.setHeight(300);
 		options.setTitle("Twacker Results for " + result.substring(result.indexOf(": \""), result.indexOf("\",")));
 		options.setPointSize(5);
 		AxisOptions opt = AxisOptions.create();
@@ -257,67 +259,6 @@ public class HomeView extends View {
 		return options;
 	}
 
-	private SelectHandler createSelectHandler(final com.google.gwt.visualization.client.visualizations.corechart.CoreChart chart) {
-		return new SelectHandler() {
-			@Override
-			public void onSelect(SelectEvent event) {
-				String message = "";
-
-				// May be multiple selections.
-				JsArray<Selection> selections = chart.getSelections();
-
-				for (int i = 0; i < selections.length(); i++) {
-					// add a new line for each selection
-					message += i == 0 ? "" : "\n";
-
-					Selection selection = selections.get(i);
-
-					if (selection.isCell()) {
-						// isCell() returns true if a cell has been selected.
-
-						// getRow() returns the row number of the selected cell.
-						int row = selection.getRow();
-						// getColumn() returns the column number of the selected cell.
-						int column = selection.getColumn();
-						message += "cell " + row + ":" + column + " selected";
-					} else if (selection.isRow()) {
-						// isRow() returns true if an entire row has been selected.
-
-						// getRow() returns the row number of the selected row.
-						int row = selection.getRow();
-						message += "row " + row + " selected";
-					} else {
-						// unreachable
-						message += "Pie chart selections should be either row selections or cell selections.";
-						message += "  Other visualizations support column selections as well.";
-					}
-				}
-
-				Window.alert(message);
-			}
-		};
-	}
-
-	private AbstractDataTable createLineGraphs(String result) {
-		String histogram = result.substring(result.indexOf("histogram"), result.indexOf("sentiment"));
-		String data = histogram.substring(histogram.indexOf("data"), histogram.indexOf("]"));
-		String[] days = data.split(",");
-		int[] counts = new int[days.length];
-		for (int i = 0; i < days.length; i++) {
-			counts[i] = Integer.parseInt(days[i].replaceAll("\\D", ""));
-		}
-
-		DataTable data2 = DataTable.create();
-		data2.addColumn(ColumnType.NUMBER, "Day");
-		data2.addColumn(ColumnType.NUMBER, "Search");
-		data2.addRows(days.length);
-		for (int i = 0; i < counts.length; i++) {
-			data2.setValue(i, 0, i);
-			data2.setValue(i, 1, counts[i]);
-		}
-		return data2;
-	}
-	
 	private AbstractDataTable createPieChart(String result) {
 		String pie = result.substring(result.indexOf("sentiment"), result.indexOf("experts"));
 		String data = pie.substring(pie.indexOf("positive"), pie.indexOf("}"));
